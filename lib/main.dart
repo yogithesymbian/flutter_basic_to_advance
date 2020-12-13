4c6d05899638854ae9fc4054d2ba47de5f2e55e4
yogithesymbian <yogirenbox33@gmail.com>
2020-12-13 10:43:32 +0800

    stateless and statefull { WIDGET }

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-// void main() {
-//   runApp(MyApp());
-// }
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+  void addNum() {
+    setState(() {
+      number += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Aplikasi Hello World"),
+        appBar: AppBar(
+          title: Text("StateFull Widget"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 11 + number.toDouble()),
+              ),
+              RaisedButton(
+                child: Text("Add Number"),
+                onPressed: addNum,
+              )
+            ],
           ),
-          body: Container(
-            color: Colors.lightBlueAccent,
-            margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            padding: EdgeInsets.all(20),
-            child: Container(
-              // margin: EdgeInsets.only(bottom: 20, top: 20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomLeft,
-                      colors: [Colors.amber, Colors.blue])),
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
