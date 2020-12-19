27da55aa14af7b376b7dc4263cd571bd04bf41e8
yogithesymbian <yogirenbox33@gmail.com>
2020-12-20 06:33:43 +0800

    design_ui [container added]

--- lib/screen/home/components/body.dart
+++ lib/screen/home/components/body.dart
@@ -15,7 +15,33 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearchBox(size: size, txtWelcome: "Hi Yogi!"),
-          TitleUnderlineWithMoreBtn(txtTitle: "Recommended", txtBtn: "More")
+          TitleUnderlineWithMoreBtn(
+            txtTitle: "Recommended",
+            txtBtn: "More",
+            press: () {},
+          ),
+          // it will cover 40% of our total width
+          Container(
+            margin: EdgeInsets.only(
+                left: kDefaultPadding,
+                top: kDefaultPadding / 2,
+                bottom: kDefaultPadding * 2.5),
+            width: size.width * 0.4,
+            child: Column(
+              children: [
+                Image.asset("assets/images/image_1.png"),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding / 2),
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23))
+                  ]),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
