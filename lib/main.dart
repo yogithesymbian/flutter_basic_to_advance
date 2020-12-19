b8c0155d07fe9324be428afcd08b412c37d24405
yogithesymbian <yogirenbox33@gmail.com>
2020-12-19 13:08:18 +0800

    FLEXIBLE LAYOUT

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Animated Contaienr"),
+          title: Text("Flexible Layout"),
         ),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {});
-            },
-            child: AnimatedContainer(
-              color: Color.fromARGB(255, random.nextInt(256),
-                  random.nextInt(256), random.nextInt(256)),
-              duration: Duration(seconds: 1),
-              width: 50.0 + random.nextInt(101),
-              height: 50.0 + random.nextInt(101),
+        body: Column(
+          children: [
+            Flexible(
+              child: Row(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.red,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.green,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.purple,
+                      )),
+                ],
+              ),
             ),
-          ),
+            Flexible(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.yellow,
+                )),
+            Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.blue,
+                )),
+          ],
         ),
       ),
     );
