85c65a1e9e9c7026f524b3067c475f351e5431f1
yogithesymbian <yogirenbox33@gmail.com>
2020-12-20 15:45:56 +0800

    design_ui [added combine card image text ]

---
+++ lib/utils/ui/combine/yaw_card_imatext.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/utils/constants.dart';
+
+class YawCardImaTxt extends StatelessWidget {
+  const YawCardImaTxt({
+    Key key,
+    this.image,
+    this.title,
+    this.subTitle,
+    this.price,
+    this.press,
+  }) : super(key: key);
+
+  final String image, title, subTitle, price;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding * 2.5),
+      width: size.width * 0.25,
+      child: Column(
+        children: [
+          Image(
+            image: AssetImage(image),
+            // width: 160,
+            // height: 190,
+          ),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              width: 160, //bad practice
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.3))
+                  ]),
+              child: Row(
+                children: [
+                  RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.button),
+                      TextSpan(
+                          text: "$subTitle".toUpperCase(),
+                          style:
+                              TextStyle(color: kPrimaryColor.withOpacity(0.5)))
+                    ]),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$$price",
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        .copyWith(color: kPrimaryColor),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
