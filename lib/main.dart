8475dbcdf0b887ab8bd862e46ce4ca0eb55bcb3f
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-07-03 13:01:49 +0800

    2021 new learning path

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-// void main() {
-//   runApp(MyApp());
-// }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aplikasi Hello World"),
-        ),
-        body: Center(
-            child: Container(
-                color: Colors.lightBlue,
-                width: 150,
-                height: 100,
-                child: Center(
-                    child: Text(
-                  "Saya sedang melatih kemampuan flutter saya",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20),
-                )))),
-      ),
+          appBar: AppBar(title: Text("Aplikasi Hello World")),
+          body: Text('hello world')),
     );
   }
 }
