22218eb7fc544476766f4637bb7f73e7efebcdaf
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-07-04 15:26:58 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: Text("Aplikasi Hello World")),
-          body: Center(child: Text('hello world'))),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.inactive) {
+      print("====> APPS IN INACTIVE <====");
+    } else if (state == AppLifecycleState.paused) {
+      print("====> APPS IN PAUSED <====");
+    } else if (state == AppLifecycleState.resumed) {
+      print("====> APPS IN RESUMED <====");
+    } else {
+      print("====> none <====");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Application site demo"),
+      ),
+      body: Container(),
     );
   }
 }
