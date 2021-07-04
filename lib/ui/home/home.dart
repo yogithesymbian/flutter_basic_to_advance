3fb6e37c4dee44829814e3c9f8ac70696a913393
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-07-04 19:57:48 +0800

    extract method

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -30,45 +30,93 @@ class _ListCourseYtPageState extends State<ListCourseYtPage> {
         child: Center(
           child: Column(
             children: [
-              SizedBox(
-                width: double.infinity,
-                child: RaisedButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  child: Text(
-                    "First Application",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: RaisedButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    Navigator.pushNamed(context, lifeCyclePageRoute);
-                  },
-                  child: Text(
-                    "Application Life Cycle state",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: RaisedButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  child: Text(
-                    "SplashScreen Android & IOS",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              )
+              FlutterBasicKnowledge(),
+              Divider(),
+              FlutterLayoutBasicWidgets(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class FlutterLayoutBasicWidgets extends StatelessWidget {
+  const FlutterLayoutBasicWidgets({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "Flutter Layout + Basic Widgets",
+          style: TextStyle(
+              fontSize: 15, fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
+      ],
+    );
+  }
+}
+
+class FlutterBasicKnowledge extends StatelessWidget {
+  const FlutterBasicKnowledge({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "Flutter Basic Knowledge",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
+            ),
+          ],
+        ),
+        Divider(),
+        SizedBox(
+          width: double.infinity,
+          child: RaisedButton(
+            color: Colors.blue,
+            onPressed: () {},
+            child: Text(
+              "First Application",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: RaisedButton(
+            color: Colors.blue,
+            onPressed: () {
+              Navigator.pushNamed(context, lifeCyclePageRoute);
+            },
+            child: Text(
+              "Application Life Cycle state",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: RaisedButton(
+            color: Colors.blue,
+            onPressed: () {},
+            child: Text(
+              "SplashScreen Android & IOS",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
